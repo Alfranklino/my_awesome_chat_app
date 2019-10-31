05542770baffa468d0f151a76b4a76cb63244e46
Alfranklino <alfranklino@gmail.com>
2019-10-31 14:20:26 -0400

    Chore: Schema completed.

--- server/migrations/createSchema.js
+++ server/migrations/createSchema.js
@@ -38,5 +38,13 @@ exports.up = pgm => {
             "group_id" INT REFERENCES chatwithme.groups(id) ON DELETE CASCADE,
             "time_added" TIMESTAMP NOT NULL DEFAULT NOW()
         );
+    `),
+    pgm.sql(`
+        CREATE TABLE "chatwithme"."usersessions" (
+            "id" SERIAL PRIMARY KEY NOT NULL,
+            "user_id" INT REFERENCES chatwithme.users(id) ON DELETE CASCADE,
+            "time_session_starts" TIMESTAMP NOT NULL DEFAULT NOW(),
+            "time_session_ends" TIMESTAMP
+        );
     `);
 };
--- server/schema.js
+++ server/schema.js
@@ -2,13 +2,30 @@ const { gql } = require("apollo-server-express");
 
 module.exports = gql`
   #   Query, Mutation, Subscription
+
+  scalar Date
+
   type Query {
-    getAllUsers: [User]!
-    test: String
+    getUsers: [User]!
+    getUserInfo(input: QueryUserInput!): User!
+    getGroups: [Group]!
+    getGroupInfo(input: QueryGroupInput!): Group!
+    getMessages: [Message]!
+    getMessageInfo(input: QueryMessageInput!): Message!
+    getSessions: [Session]!
   }
 
   type Mutation {
-    
+    setNewUser(userInfo: UserInput!): User!
+    setNewGroup(groupInfo: GroupInput!): Group!
+    setNewMessage(messageInfo: MessageInput!): Message!
+    addMemberToGroup(memberInfo: MemberInput!): Group!
+    openSession(openSessionInfo: SessionInput!): Session!
+    closeSession(closeSessionInfo: SessionInput!): Session!
+  }
+
+  type Subscription {
+
   }
 
   #   Main Customs Types
@@ -24,27 +41,65 @@ module.exports = gql`
     date_created: String
     avatar: String
     status: String
+    messagesReceived: [Message]
+    messagesSent: [Message]
+    groups: [Group]
+    sessions: [Session]!
+  }
+
+  type Group {
+    id: ID!
+    created_by: User!
+    members: [User]!
+    group_title: String!
+    time_created: Date!
+  }
+
+  type Message {
+    id: ID!
+    content: String!
+    user_from: User!
+    user_to: User
+    group_to: Group
+    time_created: Date!
+  }
+
+  type Session {
+    id: ID!
+    user: User!
+    time_session_starts: Date
+    time_session_ends: Date
   }
 
   # Main Inputs
 
-  # User------------------------
-  input SignupInput {
+  # Queries------------------------
+  input QueryUserInput {
+    userId: INT!
+    userEmail: String
+  }
+  input QueryGroupInput {
+    creatorId: INT
+    group_title: String
+  }
+  input QueryMessageInput {
+    messageId: INT
+  }
+  #Mutations----------------------
+  input UserInput {
     fullname: String!
     password: String!
     email: String!
-    date_of_birth: String
-    phone_number: String
-    avatar: String
-    location: String
   }
-
-  input LoginInput {
-    email: String!
-    password: String!
+  input GroupInput {
+    creatorId: INT!
+    group_title: String!    
   }
-
-  input LogoutInput {
-    id: ID
+  input MemberInput {
+    userId: INT!
+    groupId: INT!
   }
-`;
+  input SessionInput {
+    userId: INT!
+  }
+  `;
